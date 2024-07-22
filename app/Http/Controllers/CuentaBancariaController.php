51ceb2cc5106c693467a50f13280d7be3d5631c0
Rosanyelis <rosanyelismendoza@gmail.com>
2024-07-22 10:29:53 -0400

    inicializacion

---
+++ app/Http/Controllers/CuentaBancariaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CuentaBancaria;
+use App\Http\Requests\StoreCuentaBancariaRequest;
+use App\Http\Requests\UpdateCuentaBancariaRequest;
+
+class CuentaBancariaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCuentaBancariaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CuentaBancaria $cuentaBancaria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CuentaBancaria $cuentaBancaria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCuentaBancariaRequest $request, CuentaBancaria $cuentaBancaria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CuentaBancaria $cuentaBancaria)
+    {
+        //
+    }
+}
